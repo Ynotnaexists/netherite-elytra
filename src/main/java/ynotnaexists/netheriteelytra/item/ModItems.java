a301e04b21f90b49015641cb14c0991a670691eb
Ynotnaexists <166554082+Ynotnaexists@users.noreply.github.com>
2024-10-16 19:30:28 +0100

    Knockback resistance, attempting item models

--- src/main/java/ynotnaexists/netheriteelytra/item/ModItems.java
+++ src/main/java/ynotnaexists/netheriteelytra/item/ModItems.java
@@ -2,9 +2,6 @@ package ynotnaexists.netheriteelytra.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ElytraItem;
-import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -16,7 +13,9 @@ import ynotnaexists.netheriteelytra.NetheriteElytra;
 public class ModItems {
 
 
-    public static final Item NETHERITE_ELYTRA = registerItem("netherite_elytra", new NetheriteElytraItem(new Item.Settings().maxDamage(472).rarity(Rarity.EPIC).fireproof()));
+    public static final Item NETHERITE_ELYTRA = registerItem("netherite_elytra", new NetheriteElytraItem(
+            new Item.Settings().maxDamage(472).rarity(Rarity.EPIC).fireproof().attributeModifiers(NetheriteElytraItem.createKnockbackModifier(0.1f, "amor.elytra"))));
+
     private static Item registerItem(String name, Item item) {
         registerModItems();
         return Registry.register(Registries.ITEM, Identifier.of(NetheriteElytra.MOD_ID, name), item);
--- src/main/java/ynotnaexists/netheriteelytra/item/NetheriteElytraItem.java
+++ src/main/java/ynotnaexists/netheriteelytra/item/NetheriteElytraItem.java
@@ -2,18 +2,12 @@ package ynotnaexists.netheriteelytra.item;
 
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ElytraItem;
 import net.minecraft.item.Equipment;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
 
 public class NetheriteElytraItem extends ElytraItem implements Equipment {
     public NetheriteElytraItem(Settings settings) {
@@ -23,32 +17,14 @@ public class NetheriteElytraItem extends ElytraItem implements Equipment {
     public EquipmentSlot getSlotType() {
         return EquipmentSlot.CHEST;
     }
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        ItemStack chestItem = ((LivingEntity) entity).getEquippedStack(EquipmentSlot.CHEST);
-        if (chestItem.isOf(ModItems.NETHERITE_ELYTRA)) {
-            modifyKnockback((LivingEntity) entity, 0.5F, "amor.elytra");
-        }
-    }
 
-    public void modifyKnockback (LivingEntity entity, float knockback, String path) {
-        EntityAttributeInstance attributeInstance = entity.getAttributeInstance(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE);
-        if (attributeInstance != null) {
-            // Remove any existing modifier with the same UUID to avoid stacking
-//            attributeInstance.removeModifier(EntityAttributeModifier.);
-            // Add the new modifier
-            attributeInstance.addTemporaryModifier(
-                new EntityAttributeModifier(Identifier.of("netheriteelytra", path), knockback, EntityAttributeModifier.Operation.ADD_VALUE)
-            );
-        }
-//
-//
-//        AttributeModifierSlot attributeModifierSlot = AttributeModifierSlot.forEquipmentSlot(EquipmentSlot.CHEST);
-//        AttributeModifiersComponent.Builder builder = AttributeModifiersComponent.builder();
-//        builder.add(
-//                EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,
-//                new EntityAttributeModifier(Identifier.of("netheriteelytra", path), (double) knockback, EntityAttributeModifier.Operation.ADD_VALUE),
-//                attributeModifierSlot
-//        );
+    public static AttributeModifiersComponent createKnockbackModifier(float knockback, String path) {
+        return AttributeModifiersComponent.builder()
+            .add(
+                    EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,
+                    new EntityAttributeModifier(Identifier.of("netheriteelytra", path), knockback, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.CHEST
+            )
+            .build();
+
     }
 }
